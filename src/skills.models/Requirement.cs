ecebf18508f2968fa8d4fa59ceb4098c58bbf863
sintutu <160270685+sintutu@users.noreply.github.com>
2024-03-24 14:58:07 +0200

    feat: :sparkles: add class library for models

---
+++ src/skills.models/Requirement.cs
@@ -0,0 +1,18 @@
+﻿namespace skills.models
+{
+    /// <summary>
+    /// Represents a software requirement.
+    /// </summary>
+    public class Requirement
+    {
+        /// <summary>
+        /// Gets or sets the title of the requirement.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of test scenarios associated with the requirement.
+        /// </summary>
+        public List<Scenario> TestScenarios { get; set; }
+    }
+}
